228617da89409c46b3439c86cdd6cd80b2df8db6
ABIPRAVI <darkparadise877@gmail.com>
2024-02-16 20:21:55 +0530

    updated CURL

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const sendMessages = async (promot, brainId, name) => {
 	messagesData.push({ user: 'user', message: promot });
 	displayMessages();
 
-	const url = `http://mita.marvelcloudsolutions.tech/chat?brain_id=${brainId}&user_name=${name}&prompt=${promot}`;
+	const url = `https://mita.onrender.com/chat?brain_id=${brainId}&user_name=${name}&prompt=${promot}`;
 	// http://mita.marvelcloudsolutions.tech/chat?brain_id=1211&user_name=praveen&prompt=wleocme
 	try {
 		const response = await fetch(url);
